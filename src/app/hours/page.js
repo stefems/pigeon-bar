32d626919947791723199b74db12d2d231783e0c
Stefan Kowal <stefan.kowal@rover.com>
2026-04-07 19:32:38 -0700

    updates

--- src/app/hours/page.js
+++ src/app/hours/page.js
@@ -1,3 +1,28 @@
+import styles from "./page.module.css";
+import Image from "next/image";
+import Link from 'next/link'
+
+
 export default function Page({ params, searchParams }) {
-    return <h1>Hours Page</h1>
+    return (
+      <div>
+        <nav className={styles.nav}>
+          <Link href="/">home</Link>
+        </nav>
+        <Image
+          loading="eager"
+          fetchPriority="high"
+          width="500" height="500"
+          alt={"hours"}
+          className={styles.hours}
+          src="/hours.png"
+        />
+        <nav  className={styles.nav}>
+          <Link href="https://maps.app.goo.gl/cRxxzn8TKhwysGsa8">location</Link>
+          <Link href="/hours">hours</Link>
+          <Link href="/menu">menu</Link>
+          <Link href="mailto:pigeondenver@gmail.com">contact</Link>
+        </nav>
+      </div>
+    )
   }
--- src/app/page.js
+++ src/app/page.js
@@ -3,58 +3,31 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 import Link from 'next/link'
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 export default function Home() {
-
-  const [gifLoading, setGifLoading] = useState(true);
-  console.log(gifLoading);
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        {/* <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        /> */}
         <div className={styles.intro}>
-          {/* home logo width="1242" height="198" */}
           <Image
-              loading="eager"
-              fetchPriority="high"
-              width="1242" height="198" 
-              // sizes="(max-width: 1080px) 100vw, 33vw"
-              alt={"geometric styling of pigeon"}
-              className={styles.logo}
-              src="/logo.jpg"
-            />
-          {/* cubes gif */}
-          {gifLoading &&
-            <Image
-              loading="eager"
-              fetchPriority="high"
-              width="500" height="500"
-              // sizes="(max-width: 1080px) 100vw, 33vw"
-              alt={"geometric animation of grid of squares"}
-              className={styles.cubes}
-              src="/cubes-static.png"
-            />
-          }
-          {/* <Image
-            width="500" height="500"
-            // sizes="(max-width: 1080px) 100vw, 33vw"
+            loading="eager"
+            fetchPriority="high"
+            width="1242" height="198" 
+            alt={"geometric styling of pigeon"}
+            className={styles.logo}
+            src="/logo.jpg"
+          />
+          <video
             alt={"geometric animation of grid of squares"}
-            className={`${styles.cubes} ${gifLoading && styles.hidden}`}
-            src="/home-cubes.gif"
-            onLoad={() => setGifLoading(false)}
-            onError={(e) => console.error(e.target.id)}
-          /> */}
-          {/* nav menu */}
+            className={`${styles.cubes}`}
+            src="/cubes.mp4"
+            controls={false}
+            autoPlay
+            muted
+            loop
+          />
           <nav  className={styles.nav}>
-            <Link href="/about">about</Link>
             <Link href="https://maps.app.goo.gl/cRxxzn8TKhwysGsa8">location</Link>
             <Link href="/hours">hours</Link>
             <Link href="/menu">menu</Link>
